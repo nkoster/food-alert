7ed3b85a7652e62a4c66691ac2b70baa6cce01c9
Niels <n.koster@portavita.eu>
2023-01-30 21:08:35 +0100

    Make fetch async/await

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,25 @@
-export function saveProduct(product) {
+export async function saveProduct(product) {
 
   const productCode = product.code;
 
   console.log('saveProduct', productCode);
 
-  fetch(`https://foodproducts.w3b.net/product`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(product)
-  })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+  const fetcher = async () => {
+    try {
+      const response = await fetch(`https://foodproducts.w3b.net/product`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product)
+      });
+      return await response.json();
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  await fetcher()
+
 }
