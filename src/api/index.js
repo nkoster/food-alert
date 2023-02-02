405c0fffcb5e6ac8c8953ecc4e6e537c8e984d12
Niels <n.koster@portavita.eu>
2023-02-02 20:11:06 +0100

    Error resilient json parser

--- src/api/index.js
+++ src/api/index.js
@@ -26,6 +26,18 @@ export async function saveProduct(product) {
 
 export async function getProduct(productCode) {
   console.log('getProduct', productCode);
-  const response = await fetch(`https://world.openfoodfacts.org/api/v0/product/${productCode}.json`);
+  let jsonObject = {};
+  try {
+    const response = await fetch(`https://world.openfoodfacts.org/api/v0/product/${productCode}.json`);
+    jsonObject = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+  return jsonObject;
+}
+
+export async function getProductW3B(productCode) {
+  console.log('getProductW3B', productCode);
+  const response = await fetch(`https://foodproducts.w3b.net/product/${productCode}.json`);
   return await response.json();
 }
