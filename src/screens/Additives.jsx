b5bb22fec4f3c63f59050570bbcb5f25fd7811a4
Niels <n.koster@portavita.eu>
2023-02-02 20:06:14 +0100

    Add back button

--- src/screens/Additives.jsx
+++ src/screens/Additives.jsx
@@ -1,11 +1,14 @@
 import {useContext} from 'react'
-import {View, Text, StyleSheet, FlatList} from 'react-native'
-import {Context} from '../context/ProductContext';
-import enummers from '../data/enummers.json';
+import {View, Text, StyleSheet, FlatList, Button} from 'react-native'
+import {useNavigation} from '@react-navigation/native'
+import {Context} from '../context/ProductContext'
+import enummers from '../data/enummers.json'
 
 export default function Additives() {
 
   const {state} = useContext(Context)
+
+  const navigation = useNavigation()
 
   const additives = Array.isArray(state.additives_tags)
     ? state.additives_tags
@@ -22,11 +25,14 @@ export default function Additives() {
           data={enummersFiltered}
           renderItem={({item}) => (
             <View style={styles.flatListItem}>
-              <Text style={styles.additiveText}>{item.code}</Text>
+              <Text style={styles.additiveCodeText}>{item.code}</Text>
               <Text style={styles.additiveText}>{item.text}</Text>
             </View>
           )}
         />
+      </View>
+      <View style={styles.backButtonView}>
+        <Button title={'Back'} onPress={() => navigation.goBack()}/>
       </View>
     </View>
   )
@@ -37,22 +43,40 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    height: '100%',
+  },
+  flatList: {
+    paddingTop: 50,
+    width: '94%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   flatListView: {
-    width: '88%',
+    paddingTop: 50,
+    width: '94%',
+    height: '100%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
   },
   flatListItem: {
-    width: '100%',
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row',
   },
+  additiveCodeText: {
+    fontSize: 16,
+    padding: 10,
+    fontWeight: 'bold',
+  },
   additiveText: {
     fontSize: 16,
     padding: 10,
+    textAlign: 'right',
+  },
+  backButtonView: {
+    bottom: 60,
   },
 })
